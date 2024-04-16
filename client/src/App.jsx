ea861bd5fd8cf47707cc3b076126216ee0734b82
Shubham Aggarwal <aggarwalshubham026@gmail.com>
2024-04-16 15:03:00 +0530

    Dashboard Update

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,7 @@ import Contact from "./Pages/Contact"
 import Login from "./Pages/Login"
 import SignUp from "./Pages/SignUp"
 import Navbar from "./components/Navbar"
+import Dashboard from "./Pages/Dashboard"
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { useDispatch, useSelector } from 'react-redux';
 import { useEffect } from 'react';
@@ -21,6 +22,7 @@ export default function App() {
       <Router>
         <Routes>
           <Route path="/" element={<Home/>} />
+          <Route path="/dashboard" element={<Dashboard />} />
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<SignUp />} />
           <Route path="/contact" element={<Contact />} />
--- client/src/Pages/Dashboard.jsx
+++ client/src/Pages/Dashboard.jsx
@@ -1,8 +1,12 @@
 import React from 'react';
-
-function Home() {
+import { useDispatch, useSelector } from 'react-redux';
+export default function Dashboard() {
+    const dispatch = useDispatch();
+    const { user } = useSelector((state) => state.user);
     return (
         <div>
+            Hello, {user.name}!
+            So, you are a {user.role === "admin" ? "Officer" : "Voter"}.
         </div>
     );
 }
