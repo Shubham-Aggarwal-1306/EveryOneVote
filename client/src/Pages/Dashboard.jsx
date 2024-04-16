f942e6aaeae15df3fe29776b35752f2fd4d93f83
Shubham Aggarwal <aggarwalshubham026@gmail.com>
2024-04-16 15:08:23 +0530

    Update in Dashboard

--- client/src/Pages/Dashboard.jsx
+++ client/src/Pages/Dashboard.jsx
@@ -4,14 +4,21 @@ export default function Dashboard() {
     const dispatch = useDispatch();
     const { user, isAuthenticated} = useSelector((state) => state.user);
     return isAuthenticated ? (
-        <div>
-            Hello, {user?.name}!
-            So, you are a {user?.role === "admin" ? "Officer" : "Voter"}.
-        </div>
+        user?.role === "user" ? (
+            <div>
+                <h1>Welcome, {user?.name}</h1>
+                <h2>Voter Dashboard</h2>
+            </div>
+        ) : (
+            <div>
+                <h1>Welcome, {user?.name}</h1>
+                <h2>Officer Dashboard</h2>
+            </div>
+        )
     ) :
     (
         <div>
-
+            
         </div>
     );
 }
