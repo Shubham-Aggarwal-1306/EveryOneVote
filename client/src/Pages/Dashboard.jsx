f39ba42fff4dc466d5c344e922aabff4081b7f5f
Shubham Aggarwal <aggarwalshubham026@gmail.com>
2024-04-16 15:06:17 +0530

    Update in Dashboard

--- client/src/Pages/Dashboard.jsx
+++ client/src/Pages/Dashboard.jsx
@@ -2,11 +2,16 @@ import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 export default function Dashboard() {
     const dispatch = useDispatch();
-    const { user } = useSelector((state) => state.user);
-    return (
+    const { user, isAuthenticated} = useSelector((state) => state.user);
+    return isAuthenticated ? (
         <div>
-            Hello, {user.name}!
-            So, you are a {user.role === "admin" ? "Officer" : "Voter"}.
+            Hello, {user?.name}!
+            So, you are a {user?.role === "admin" ? "Officer" : "Voter"}.
+        </div>
+    ) :
+    (
+        <div>
+
         </div>
     );
 }
